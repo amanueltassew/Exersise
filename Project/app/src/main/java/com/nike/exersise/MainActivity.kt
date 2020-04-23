291ae044a5ada24ebca3eef758f799b7a11bb1c3
amanueltassew <amanuel.tassew1@gmail.com>
2020-04-23 16:23:37 -0500

    Added Navigation Component
    For Home,History and Detail.

--- Project/app/src/main/java/com/nike/exersise/MainActivity.kt
+++ Project/app/src/main/java/com/nike/exersise/MainActivity.kt
@@ -2,11 +2,22 @@ package com.nike.exersise
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val navController = findNavController(R.id.nav_host_fragment);
+        val bottomNav = findViewById<BottomNavigationView>(R.id.bottom_nav)
+        bottomNav?.setupWithNavController(navController);
     }
 }
